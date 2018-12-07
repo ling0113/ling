02fc7731c843a5b22aa60feef2d6daba4abda74e
ling <lgr0113@126.com>
2018-12-07 15:33:36 +0800

    新建

---
+++ git2/src/main/java/user.java
@@ -0,0 +1,26 @@
+/**
+ * @program: git2
+ * @description:
+ * @author: ling
+ * @create: 2018-12-07 15:30
+ */
+public class user {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
